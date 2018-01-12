c93d9c8fa32f229123bb4a9090366d2f6a623112
Jeremy S. Lee <jeremysanglee@gmail.com>
2018-01-11 21:32:01 -0800

    Split CSS for better load time in production

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -7,25 +7,25 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  // plugins: [
-  //   new ExtractTextPlugin('bundle.css'),
-  // ],
+  plugins: [
+    new ExtractTextPlugin('bundle.css'),
+  ],
   module: {
     rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }
       // {
       //   test: /\.css$/,
-      //   use: ExtractTextPlugin.extract({
-      //     fallback: 'style-loader',
-      //     use: 'css-loader'
-      //   })
+      //   use: [
+      //     'style-loader',
+      //     'css-loader'
+      //   ]
       // }
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      }
     ]
   }
 };
