bbf6337c64686e6891cf89fae907feee64dbc067
Jeremy S. Lee <jeremysanglee@gmail.com>
2018-01-11 21:24:45 -0800

    Load css with css-loader and style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
 const path = require('path');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  // plugins: [
+  //   new ExtractTextPlugin('bundle.css'),
+  // ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+      // {
+      //   test: /\.css$/,
+      //   use: ExtractTextPlugin.extract({
+      //     fallback: 'style-loader',
+      //     use: 'css-loader'
+      //   })
+      // }
+    ]
   }
 };
