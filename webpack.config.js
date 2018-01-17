59c51f472de29816286658988f191c8662ac20da
Jeremy S. Lee <jeremysanglee@gmail.com>
2018-01-17 15:40:37 -0800

    Add fonts with file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,12 @@ module.exports = {
         use: [
           'file-loader'
         ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otff)$/,
+        use: [
+          'file-loader'
+        ]
       }
     ]
   }
