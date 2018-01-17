28e93f6190d6009f7e1024307ecfc4f2dbb024b2
Jeremy S. Lee <jeremysanglee@gmail.com>
2018-01-17 15:21:47 -0800

    Clean up for output management

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import Smudge from './smudge.jpg';
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'Webpack'], ' ');
-  element.classList.add('hello');
-  
-  const myIcon = new Image();
-  myIcon.src = Smudge;
-  
-  element.appendChild(myIcon);
 
   return element;
 }
