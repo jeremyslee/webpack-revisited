e6c159ec2faf886a6541ab30a47bb9f120c50ad7
Jeremy S. Lee <jeremysanglee@gmail.com>
2018-01-17 16:21:35 -0800

    Add print.js update entry point and output of webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,12 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   }
 };
