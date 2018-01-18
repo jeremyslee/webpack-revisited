b409980fcdd18d2d0edc2aa3d96093b16ae3a34a
Jeremy S. Lee <jeremysanglee@gmail.com>
2018-01-17 16:26:48 -0800

    Install and add HtmlWebpackPlugin to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
     app: './src/index.js',
     print: './src/print.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack Revisited'
+    })
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
